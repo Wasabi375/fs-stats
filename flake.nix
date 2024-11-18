60a8d79595daa0de1e0f6a3564b0a2b4a17205b6
Burkhard Mittelbach <wasabi37a@googlemail.com>
2024-11-18 01:32:38 +0100

    Nix: build package using crane

--- flake.nix
+++ flake.nix
@@ -12,8 +12,8 @@
             inputs.nixpkgs-lib.follows = "nixpkgs";
         };
         nix-filter.url = "github:numtide/nix-filter";
+        crane.url = "github:ipetkov/crane";
     };
-
 #            overlays = [
 #                rust-overlay.overlays.default
 #                (final: prev: {
@@ -33,12 +33,17 @@
 #            ];
 
 
-    outputs = inputs@{ self, nixpkgs, rust-overlay, parts, nix-filter }:
+    outputs = inputs@{ self, nixpkgs, rust-overlay, parts, nix-filter, crane }:
     parts.lib.mkFlake { inherit inputs; } {
         systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
         perSystem = { self', lib, system, ... }: 
         let 
-            pkgs = nixpkgs.legacyPackages.${system};
+            overlays = [ (import rust-overlay)];
+            pkgs = import nixpkgs { inherit system; overlays = overlays; };
+            rust-toolchain = if builtins.pathExists ./rust-toolchain.toml then 
+                pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+            else 
+                pkgs.rust-bin.stable.latest;
             cargo_toml = builtins.fromTOML( builtins.readFile ./Cargo.toml);
             dev_tools = with pkgs; [
                 pkg-config
@@ -48,11 +53,11 @@
                 rust-analyzer
                 rustfmt
             ];
-        in
-        {
-            packages.default = pkgs.rustPlatform.buildRustPackage {
+            craneLib = (crane.mkLib pkgs).overrideToolchain rust-toolchain;
+            craneArgs = {
                 pname = cargo_toml.package.name;
                 version = cargo_toml.package.version;
+
                 src = nix-filter.lib.filter {
                     root = ./.;
                     include = [
@@ -60,18 +65,30 @@
                         ./Cargo.toml
                         ./Cargo.lock
                         ./rust-toolchain.toml
-                        ./foo
                     ];
                 };
 
-                cargoLock = {
-                    lockFile = ./Cargo.lock;
-                    allowBuiltinFetchGit = lib.mkDefault false;
-                };
+                nativeBuildInputs = [];
+
+                buildInputs = [];
+
+                runtimeDependencies = [];
             };
+            cargoArtifacts = craneLib.buildDepsOnly craneArgs;
+            finalPkg = craneLib.buildPackage ( craneArgs // { inherit cargoArtifacts; });
+
+        in
+        {
+            checks.finalPkg = finalPkg;
+            packages.default = finalPkg;
+
             devShells.default = pkgs.mkShell {
-                inputsFrom = [ self'.packages.default ];
                 packages = dev_tools;
+                LD_LIBRARY_PATH = lib.makeLibraryPath (
+                    __concatMap (d: d.runtimeDependencies) (__attrValues self'.checks)
+                );
+
+                inputsFrom = [ finalPkg ];
             };
         };
     };
